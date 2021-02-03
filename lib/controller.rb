8c53ed8c81c07e18c928e3619f547f5841046221
Jordan <jordancaley10@gmail.com>
2021-02-03 15:50:32 +0100

    Redirect to home page after submitting gossip

--- lib/controller.rb
+++ lib/controller.rb
@@ -11,14 +11,6 @@ class ApplicationController < Sinatra::Base
 
   post '/gossips/new/' do
     Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
   end
-
-  # post '/gossips/new/' do
-  #   puts "Hi, I'm the server"
-  #   puts "This is the content of the params hash: #{params}"
-  #   puts "Cool! And this is what the user entered for gossip_author: #{params["gossip_author"]}"
-  #   puts "Awesome, and this is waht the user entered in the gossip_content field: #{params["gossip_content"]}"
-  #   puts "That's bomb.com. I'm out!"
-  # end
-
 end
